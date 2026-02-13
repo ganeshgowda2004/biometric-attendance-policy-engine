998815aa24ec8bb9c0f5d6d824d630381860bbe6
varsha13vb <varsha.basavanakoti@gmail.com>
2026-02-13 15:23:25 +0530

    empty files

--- backend/app/models/test_model.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-class TestConnection(db.Model):
-    __tablename__ = "test_connection"
-
-    id = db.Column(db.Integer, primary_key=True)
-    message = db.Column(db.String(100))
